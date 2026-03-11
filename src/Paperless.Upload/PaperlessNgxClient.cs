bc150551b33c5d79f25ed1b78ffd6f76bb88a2b7
Oliver Schmidt <oliverpierre.schmidt@gmail.com>
2026-03-11 17:37:42 +0100

    add src

---
+++ src/Paperless.Upload/PaperlessNgxClient.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Xml.Linq;
+using System.Threading;
+
+namespace PaperLess.Upload
+{
+    public class PaperlessNgxClient
+    {
+        private string target;
+        private string token;
+
+        public PaperlessNgxClient(string target, string token)
+        {
+            this.target = target;
+            this.token = token;
+        }
+
+        public bool UploadDocument(string file, int dateTagId)
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Token", token);
+
+            client.Timeout = TimeSpan.FromSeconds(30);
+
+            using var content = new MultipartFormDataContent();
+            
+            var values = new[]
+            {
+                new KeyValuePair<string, string>("tags", dateTagId.ToString()),
+            };
+
+            foreach (var keyValuePair in values)
+            {
+                content.Add(new StringContent(keyValuePair.Value), keyValuePair.Key);
+            }
+            var fi = new System.IO.FileInfo(file);
+            if (fi.Length <= 0)
+                return false;
+
+            content.Add(new ByteArrayContent(System.IO.File.ReadAllBytes(file)), "document", fi.Name);
+
+            var requestUri = $"{target}/api/documents/post_document/";
+            var result = client.PostAsync(requestUri, content).GetAwaiter().GetResult();
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else
+            {
+                var errorContent = result.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Console.Write($" Error: {result.StatusCode}");
+                Console.WriteLine($" {errorContent}");
+                return false;
+            }
+        
+        }
+
+        public int CreateTag(string tag)
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Token", token);
+
+            var formContent = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("name", tag),
+            });
+
+
+            var response = client.PostAsync($"{target}/api/tags/", formContent).GetAwaiter().GetResult();
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var errorContent = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Console.Write($" Error: {response.StatusCode}");
+                Console.WriteLine($" {errorContent}");
+            }
+            var tagResult = response.Content.ReadFromJsonAsync<Tags.Result>();
+            return tagResult.Result.id;
+        }
+
+        public Dictionary<string, int> GetTags()
+        {
+            Dictionary<string, int> result = new();
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Token", token);
+            var url = $"{target}/api/tags/";
+
+            while (!string.IsNullOrWhiteSpace(url))
+            {
+                var response = client.GetFromJsonAsync<Tags>(url).GetAwaiter().GetResult();
+                url = response.next?.ToString();
+                var list = response.results.Select(t => new { t.name, t.id });
+                foreach (var kvp in list)
+                {
+                    result[kvp.name] = kvp.id;
+                }
+            }
+            return result;
+           
+        }
+
+
+        public class Tags
+        {
+            public int count { get; set; }
+            public object next { get; set; }
+            public object previous { get; set; }
+            public Result[] results { get; set; }
+
+            public class Result
+            {
+                public int id { get; set; }
+                public string slug { get; set; }
+                public string name { get; set; }
+                public int colour { get; set; }
+                public string match { get; set; }
+                public int matching_algorithm { get; set; }
+                public bool is_insensitive { get; set; }
+                public bool is_inbox_tag { get; set; }
+                public int document_count { get; set; }
+            }
+        }
+
+        
+    }
+}
---
+++ src/Paperless.Upload/Program.cs
@@ -0,0 +1,106 @@
+﻿using PaperLess.Upload;
+using Serilog.Events;
+using Serilog;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json")
+    .Build();
+
+IConfigurationSection configSection = config.GetSection("Settings");
+
+if (string.IsNullOrWhiteSpace(configSection["ApiUrl"]) || string.IsNullOrWhiteSpace(configSection["Token"]))
+    Console.WriteLine("The appsettings.json file is incorrect!");
+    
+
+Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+var result = DoUpload(args, configSection);
+
+if (result != 0)
+{
+    Console.WriteLine("press enter to exit");
+    Console.ReadLine();
+}
+return result;
+
+static int DoUpload(string[] args, IConfigurationSection configSection)
+{
+    if (!args.Any())
+    {
+        Log.Error("No arguments !");
+        return 1;
+    }
+
+    var file = args.First();
+    Console.WriteLine("Uploading file: " + file);
+
+    var target = configSection["ApiUrl"];
+    var token = configSection["Token"]; 
+
+    if (!File.Exists(file))
+    {
+        Log.Error($"File {file} does not exist");
+        return 1;
+    }
+    if (string.IsNullOrWhiteSpace(token))
+    {
+        Log.Error($"Token is required");
+        return 1;
+    }
+
+    var client = new PaperlessNgxClient(target, token);
+
+
+    var tagIds = client.GetTags();
+
+    var fi = new FileInfo(file);
+    var now = DateTime.Now;
+    var tag = $"{now.Year}-{now.Month.ToString("00")}";
+
+    //if(args.Length > 1)
+    //{
+       Console.WriteLine($"Use tag [{tag}] press enter or change it:");
+       var readline = Console.ReadLine();
+       if(!string.IsNullOrWhiteSpace(readline))
+       {
+           tag = readline;
+       }
+    //}
+
+    if (!tagIds.ContainsKey(tag))
+    {
+        var id = client.CreateTag(tag);
+        if (id != default)
+        {
+            tagIds[tag] = id;
+        }
+    }
+
+    if (!tagIds.ContainsKey(tag))
+    {
+        Log.Error($"Failed to create tag {tag}");
+        return 1;
+    }
+    Console.Write($"\nUploading: {fi.Name} ");
+    if (client.UploadDocument(file, tagIds[tag]))
+    {
+        Console.Write('\u2705');
+        return 0;
+    }
+    else
+    {
+        Console.Write('\u274C');
+        return 1;
+    }
+}
+
